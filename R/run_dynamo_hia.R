94af3bb2ea99806b0bdc93e4c8fed60827d47fe4
maltelueken <malte_lueken@arcor.de>
2024-11-12 16:59:54 +0100

    Update run_dynamo_hia function to work with new Java app

--- R/run_dynamo_hia.R
+++ R/run_dynamo_hia.R
@@ -1,60 +1,30 @@
 #' Run the DYNAMO-HIA simulation
 #'
-#' A wrapper for the Java application. Runs the application in batch mode.
+#' A wrapper for running the Java application. Runs the application in batch mode.
 #'
-#' @param simulation_filename Path to the simulation batch file.
-#' Must be relative to \code{app_dirname}.
-#' @param app_dirname Path to the directory of the Java application.
-#' @param app_filename Name of Java application in \code{app_dirname}.
-#' @param init_memory Initial allocated memory.
-#' Can be \code{NULL} for default of 256M.
-#' @param max_memory Maximum allocated memory.
-#' Choosing more memory than the system provides will result in an error.
+#' @param simulation_filename Path to the simulation batch file. Each line of this file must contain
+#' a directory name of a simulation that should be run.
+#' @param app_filename Path to the Java application.
+#' @param log_filename Path to a log file.
 #'
-#' @return Returns \code{TRUE} if the simulations ran successfully.
+#' @return Returns \code{TRUE} if the simulations ran successfully. Otherwise, it will forward the
+#' exception returned by the Java application
 #' @export
 #'
-#' @example TODO
-run_dynamo_hia <- function(simulation_filename,
-                           app_dirname,
-                           app_filename = "CZM_Mono_2.0.8.jar",
-                           init_memory = NULL,
-                           max_memory = "256M") {
-  # TODO: It seems like there is no schema validation in Java, should this be done here?
-  app_command <- "java"
-
-  command_args <- c(
-    paste0("-Xmx", max_memory),
-    "-jar",
-    app_filename,
-    simulation_filename
+#' @example
+#' \dontrun{
+#' run_dynamo_hia("simulation_batch.txt", "DYNAMO-HIA")
+#' }
+run_dynamo_hia <- function(simulation_filename, app_filename, log_filename = "run_dynamo_hia.log") {
+  output <- system2(
+    command = app_filename,
+    args = simulation_filename,
+    stdout = "run_dynamo_hia.log",
+    stderr = TRUE
   )
 
-  if (!is.null(init_memory)) {
-    command_args <- c(paste0("-Xms", init_memory), command_args)
-  }
-
-  suppressWarnings({
-    output <- withr::with_dir(
-      app_dirname,
-      system2(
-        command = app_command,
-        args = command_args,
-        stdout = TRUE,
-        stderr = TRUE
-      )
-    )
-  })
-
-  status <- attr(output, "status")
-
-  if (!is.null(status)) {
+  if (length(output) > 0) {
     stop(output)
-  }
-
-  # TODO: Explicitly throw error in Java program when `simulation_filename` does not exist?
-  for (msg in grep("WARN", output, value = TRUE)) {
-    warning(gsub(".*<(.*)>.*", "\\1", msg))
   }
 
   return(TRUE)
