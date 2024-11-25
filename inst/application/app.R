c6ee436f23e37a5d85f9444b418ba51b968edf79
Flavio Hafner <55716026+f-hafner@users.noreply.github.com>
2024-11-25 13:30:40 +0100

    Add disease selection tab

--- inst/application/app.R
+++ inst/application/app.R
@@ -14,7 +14,12 @@ ui <- fluidPage(
                           simulation_config_ui("simulation_config")
                         )
                       )
-             )
+             ),
+             tabPanel("Diseases",
+                      uiOutput("disease_selection_ui"),
+                      verbatimTextOutput("selected_diseases_display")
+                      )
+
   )
 )
 
@@ -41,6 +46,28 @@ server <- function(input, output, session) {
   observeEvent(user_program_config$dynamo_path(), {
     new_dynamo_path <- user_program_config$dynamo_path()
     user_paths$dynamo_path <- new_dynamo_path
+  })
+
+  output$disease_selection_ui <- renderUI({
+    req(reference_data())
+    disease_selection_ui("selection1", reference_data)
+  })
+
+  selected_diseases <- disease_selection_server("selection1", reference_data)
+
+  # without this button, selected_diseases_display updates automatically
+  #module_selection_inputs <- eventReactive(input$update_data, {
+  #  selected_diseases()
+  #})
+
+  #observeEvent(input$update_data, {
+  #  message("loaded")
+  #  output$selected_diseases_display <- renderPrint({ selected_diseases() })
+  #})
+
+  # this is for debugging at the moment
+  observeEvent(selected_diseases(), {
+    output$selected_diseases_display <- renderPrint({ selected_diseases() })
   })
 
 }
